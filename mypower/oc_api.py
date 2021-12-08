a3fd77b6bc662438bb1adaeb0af2fb664c3092f7
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2021-12-08 09:48:01 +0700

    better matpower path error handling

--- mypower/oc_api.py
+++ mypower/oc_api.py
@@ -14,22 +14,33 @@ def oc_addpath(path,oc=None):
     return oc
 
 def oc_matpower(path_matpower='matpower',oc=None):
+    error_path = False
     if path_matpower == 'matpower':
-        if os.path.isdir(os.path.join(os.path.dirname(__file__ ),path_matpower)):
-            path_matpower = os.path.join(os.path.dirname(__file__ ),path_matpower)
+        if os.path.isdir(os.path.join(os.path.dirname(__file__ ), path_matpower)):
+            path_matpower = os.path.join(os.path.dirname(__file__ ), path_matpower)
         else:
-            raise ValueError("""NO MATPOWER PATH FOUND!
-PLEASE USE:
-    oc = oc_matpower(path_matpower='/PATH/matpower')
+            error_path = True
+    elif not os.path.isdir(path_matpower):
+        error_path = True
+    
+    if error_path:
+        raise ValueError(
+            "NO MATPOWER PATH FOUND!\n"
+        """
+            PLEASE USE:
+                oc = oc_matpower(path_matpower='/PATH/TO/matpower')
 
-ALTERNATIVELY, PLACE MATPOWER IN myPower PACKAGE. 
-NAME THE FOLDER WITH 'matpower'. RESULT WILL BE:
-    ...\\mypower\\api.py
-    ...
-    ...\\mypower\\matpower << here (whithout making new matpower folder)
-    ...\\mypower\\matpower\\data
-    ...\\mypower\\matpower\\lib""")
+            ALTERNATIVELY, PLACE MATPOWER IN myPower PACKAGE and use oc = oc_matpower(). 
+            NAME THE FOLDER WITH 'matpower'. RESULT WILL BE:
+                ...\\mypower\\__init__.py
+                ...\\mypower\\matpower_api.py
+                ...
+                ...\\mypower\\matpower << here (whithout making new matpower folder)
+                ...\\mypower\\matpower\\data
+                ...\\mypower\\matpower\\lib
+        """
+    )
     if oc == None:
         oc = Oct2Py()
-    oc.addpath(oc.genpath(path_matpower))
+    oc.addpath(oc.genpath(path_matpower)) # add path to matpower
     return oc
