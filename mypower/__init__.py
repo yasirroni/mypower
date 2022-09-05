c6ebe8a3837b3c7d6367a83ce82007ab5a06e125
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-09-05 09:05:05 +0700

    add get_losses to init

--- mypower/__init__.py
+++ mypower/__init__.py
@@ -1,6 +1,7 @@
 from .version import __version__
 
 from .get_index import get_index, get_index_dynamic
+from .get_losses import get_losses, get_losses_p, get_losses_q
 from .makeB_kron import makeB_kron
 from .losses_kron import losses_kron, losses_kron_detailed
 from .oc_api import oc_addgenpath, oc_addpath, start_matpower
